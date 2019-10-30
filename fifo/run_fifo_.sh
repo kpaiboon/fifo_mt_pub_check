d646a08824f1b2f79908ab65f2ecf5c7493bd900
kpaiboon <kpaiboon@gmail.com>
2019-10-30 13:10:59 +0700

    Add files via upload

---
+++ fifo/run_fifo_.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# with local MQTT
+#python3  8pyprox.mqtt.py -I 0.0.0.0  -v -t -M 127.0.0.1 -o canon 40063 54.169.38.75 30240
+
+python3  8pyprox.mqtt.py -I 0.0.0.0  -v -t -o canon 40063 54.169.38.75 30240
